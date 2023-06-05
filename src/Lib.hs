8b05860a11a5290084e36c4f4a5ac455e4324b48
DanteBonino <43711055+DanteBonino@users.noreply.github.com>
2023-06-05 16:46:38 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,27 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib () where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Pista = [Tramo]
+type Tramo = (Int, (Chocobo -> Chocobo))
+
+data Chocobo = Chocobo {
+    fuerza      :: Int,
+    peso        :: Int,
+    velocidad   :: Int,
+    color       :: String
+}
+
+data Jinete = Jinete {
+    nombre  :: String,
+    chocobo :: Chocobo
+}
+
+--Punto 1:
+mayorSegun :: (Ord b) => (a -> b) -> a -> a -> Bool
+mayorSegun = esDeCiertaFormaSegun (>)
+
+menorSegun :: (Ord b) => (a -> b) -> a -> a -> Bool
+menorSegun = esDeCiertaFormaSegun (<)
+
+--No se me ocurre un buen Nombre para la abstracción, pero:
+esDeCiertaFormaSegun :: (Ord b) => (b -> b -> Bool) ->(a -> b) ->  a -> a -> Bool
+esDeCiertaFormaSegun unaCondicion transformador  unValor = (unaCondicion (transformador unValor) . transformador)
