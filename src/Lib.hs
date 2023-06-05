02846038b7ecae09e6fad2e7c0f24529584edcd7
DanteBonino <43711055+DanteBonino@users.noreply.github.com>
2023-06-05 17:15:21 -0300

    Punto 2

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,7 @@
 module Lib () where
 
 type Pista = [Tramo]
-type Tramo = (Int, (Chocobo -> Chocobo))
+type Tramo = (Int, (Chocobo -> Int))
 
 data Chocobo = Chocobo {
     fuerza      :: Int,
@@ -25,3 +25,20 @@ menorSegun = esDeCiertaFormaSegun (<)
 --No se me ocurre un buen Nombre para la abstracción, pero:
 esDeCiertaFormaSegun :: (Ord b) => (b -> b -> Bool) ->(a -> b) ->  a -> a -> Bool
 esDeCiertaFormaSegun unaCondicion transformador  unValor = (unaCondicion (transformador unValor) . transformador)
+
+--Punto 2:
+tiempoQueTardaEnElTramo :: Tramo -> Chocobo -> Int
+tiempoQueTardaEnElTramo  unTramo = (div (distancia unTramo) . correccionDeVelocidad unTramo)
+
+--Se podría usar fst y snd de una, pero es más claro qué está haciendo
+distancia :: Tramo -> Int
+distancia = fst
+
+correccionDeVelocidad :: Tramo -> (Chocobo -> Int)
+correccionDeVelocidad = snd
+
+--b
+tiempoTotal :: Chocobo -> Pista -> Int
+tiempoTotal  unChocobo = sum . map (flip tiempoQueTardaEnElTramo unChocobo)
+
+
